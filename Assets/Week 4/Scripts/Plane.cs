67ed1839ba31b62fbef07389a20e007ccfc48468
Maksym Kvitka <kvitka@shernet.sheridancollege.ca>
2024-02-01 10:28:08 -0500

    Task 2 - Trail done

--- Assets/Week 4/Scripts/Plane.cs
+++ Assets/Week 4/Scripts/Plane.cs
@@ -6,14 +6,49 @@ public class Plane : MonoBehaviour
 {
     public List<Vector2> points;
     public float newPositionThreshhold = 0.2f;
+    public float speed = 1;
     Vector2 lastPosition;
+    Vector2 currentPosition;
     LineRenderer lineRenderer;
+    Rigidbody2D rigibody;
 
     private void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
+        rigibody = GetComponent<Rigidbody2D>();
+
         lineRenderer.positionCount = 1;
         lineRenderer.SetPosition(0,transform.position);
+    }
+
+    private void FixedUpdate()
+    {
+        currentPosition = transform.position;
+        if(points.Count > 0)
+        {
+            Vector2 direction = points[0] - currentPosition;
+            float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+            rigibody.rotation = -angle;
+        }
+        rigibody.MovePosition(rigibody.position + (Vector2)transform.up * speed * Time.deltaTime);
+    }
+
+    private void Update()
+    {
+        lineRenderer.SetPosition(0, transform.position);
+        if (points.Count > 0)
+        {
+            if(Vector2.Distance(currentPosition, points[0]) < newPositionThreshhold)
+            {
+                points.RemoveAt(0);
+
+                for(int i = 0; i < lineRenderer.positionCount - 2; i++)
+                {
+                    lineRenderer.SetPosition(i, lineRenderer.GetPosition(i + 1));
+                }
+                lineRenderer.positionCount--;
+            }
+        }
     }
     private void OnMouseDown()
     {
