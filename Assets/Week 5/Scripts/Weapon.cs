484c3df06c2de181744f0f7dd9dbe696c6d15405
Shiro <fl34un@gmail.com>
2024-02-08 18:25:55 -0500

    Task 4a done

---
+++ Assets/Week 5/Scripts/Weapon.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    Rigidbody2D rigibody;
+
+    public float speed = 4;
+    float timer = 0;
+    float maxTime = 5;
+    void Start()
+    {
+        rigibody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        rigibody.MovePosition(rigibody.position + (Vector2)transform.up * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.gameObject.SendMessage("takeDamage", 1);
+        Destroy(gameObject);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > maxTime)
+        {
+            Destroy(gameObject);
+        }
+
+
+        rigibody.transform.position = transform.position;
+    }
+}
